f6bbf70c08fc2750cebe5e6761e19d779dc5d8d6
katayama <tattu.0310@gmail.com>
2022-05-27 23:12:16 +0900

    ⚡️ plus data

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,10 +9,38 @@ import { useForm } from "@mantine/form";
 import { Songs } from "@components/layout/Songs";
 import { useLocale } from "@hooks/useLocale";
 import { result } from "@type/typeResult";
+import useSWR from "swr";
 
+// const fetcher = async (input: RequestInfo, init: RequestInit) => {
+//   const res = await fetch(input, init);
+//   return res.json();
+// };
+
+// const useFetch = (
+//   values: { music: string },
+//   lang: "ja_jp" | "en_us",
+//   country: "jp" | "us"
+// ) => {
+//   const { data, error } = useSWR(
+//     `//itunes.apple.com/search?term=${values.music}&country=${country}&lang=${lang}&media=music&limit=51&offset=0`,
+//     fetcher
+//   );
+//   return {
+//     data,
+//     error,
+//   };
+// };
+
+const LIMIT = 21;
 const Home: NextPage = () => {
   const [loadingFlag, setLoadingFlag] = useState<boolean>(false);
   const [songsData, setSongsData] = useState<result>();
+  //-------------------------------
+  const [music, setMusic] = useState<string>("");
+  const [lang, setLang] = useState<string>("");
+  const [country, setCountry] = useState<string>("");
+  const [num, setNum] = useState<number>(0);
+  //-------------------------------
   const router = useRouter();
 
   let API_lang: "ja_jp" | "en_us" = "en_us";
@@ -37,15 +65,35 @@ const Home: NextPage = () => {
       country: "jp" | "us"
     ) => {
       setLoadingFlag(true);
+
+      setMusic(values.music);
+      setLang(lang);
+      setCountry(country);
+
       const { data } = await axios.get(
-        `//itunes.apple.com/search?term=${values.music}&country=${country}&lang=${lang}&media=music&limit=51&offset=0`
+        `//itunes.apple.com/search?term=${values.music}&country=${country}&lang=${lang}&media=music&limit=${LIMIT}&offset=0`
       );
-      console.log(data);
+
       setSongsData(data);
       setLoadingFlag(false);
     },
     []
   );
+
+  const addCard = async (numb: number) => {
+    setNum(numb);
+    const { data } = await axios.get(
+      `//itunes.apple.com/search?term=${music}&country=${country}&lang=${lang}&media=music&limit=12&offset=${
+        LIMIT + num
+      }`
+    );
+    console.log(num, [...data.results]);
+    console.log(num, data.results);
+    console.log("songdata", [...songsData!.results]);
+    //console.log("songdata", [...songsData?.results]);
+    //setSongsData([...data.results, ...songsData]);
+    //setSongsData([...songsData!.results, ...data.results]);
+  };
 
   return (
     <div className="flex flex-col justify-center">
@@ -69,9 +117,9 @@ const Home: NextPage = () => {
       />
       <div className="mt-5">
         <Songs songsData={songsData!} loading={loadingFlag} />
-        {/* <Button color="cyan" className="m-5">
+        <Button color="cyan" className="m-5" onClick={() => addCard(10)}>
           show more
-        </Button> */}
+        </Button>
       </div>
     </div>
   );
---
+++ src/pages/test.jsx
@@ -0,0 +1,59 @@
+import { useState } from "react";
+import InfiniteScroll from "react-infinite-scroller";
+
+export default function Index() {
+  const [list, setList] = useState([]); //表示するデータ
+  const [hasMore, setHasMore] = useState(true); //再読み込み判定
+
+  //項目を読み込むときのコールバック
+  const loadMore = async (page) => {
+    const response = await fetch(
+      `https://jsonplaceholder.typicode.com/todos/${page}`
+    ); //API通信
+    const data = await response.json(); //取得データ
+
+    //データ件数が0件の場合、処理終了
+    if (data.length < 1) {
+      setHasMore(false);
+      return;
+    }
+    //取得データをリストに追加
+    setList([...list, data]);
+  };
+
+  //各スクロール要素
+  const items = (
+    <ul>
+      {list.map((value, index) => (
+        <li key={index}>{value}</li>
+      ))}
+    </ul>
+  );
+
+  //全体のスタイル
+  const root_style = {
+    marginLeft: "50px",
+    marginTop: "50px",
+  };
+
+  //ロード中に表示する項目
+  const loader = (
+    <div className="loader" key={0}>
+      Loading ...
+    </div>
+  );
+
+  return (
+    <div style={root_style}>
+      <InfiniteScroll
+        loadMore={loadMore} //項目を読み込む際に処理するコールバック関数
+        hasMore={hasMore} //読み込みを行うかどうかの判定
+        loader={loader}
+      >
+        {" "}
+        {/* 読み込み最中に表示する項目 */}
+        {items} {/* 無限スクロールで表示する項目 */}
+      </InfiniteScroll>
+    </div>
+  );
+}
